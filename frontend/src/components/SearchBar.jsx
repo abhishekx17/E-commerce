b82eb98546387630f272dc80970095415df75dec
Abhishek <abhishek.0x17@gmail.com>
2026-07-05 21:06:28 +0530

    starting ecommerce project

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import { assets } from "assets/assets";
+import { ShopContext } from "context/ShopContext";
+import React, { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const SearchBar = () => {
+  const { search, setSearch, setShowSearch, showSearch } =
+    useContext(ShopContext);
+    const  location = useLocation()
+    const [visible,setVisible] = useState(false)
+
+    useEffect(()=>{
+        if(location.pathname.includes('collection')){
+                setVisible(true)
+        }else{
+            setVisible(false)
+        }
+    },[location])
+
+  return showSearch && visible ? (
+    <div className="border-t border-b bg-gray-50 text-center">
+      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="flex-1 outline-none bg-inherit text-sm"
+          placeholder="Search"
+        />
+        <img src={assets.search_icon} alt="" className="w-4" />
+      </div>
+      <img src={assets.cross_icon} alt="" className="inline w-3 cursor-pointer" onClick={()=>setShowSearch(false)} />
+    </div>
+  ) : null;
+};
+
+export default SearchBar;
