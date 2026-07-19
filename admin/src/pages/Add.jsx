3de874eb00990624bf041839ce1ab8f279485e2d
Abhishek <abhishek.0x17@gmail.com>
2026-07-20 03:23:21 +0530

    bug fixed

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -35,10 +35,10 @@ const Add = ({ token }) => {
       formData.append("bestseller", bestseller);
       formData.append("sizes", JSON.stringify(sizes));
 
-      image1 && formData.append("image1", image1);
-      image2 && formData.append("image2", image2);
-      image3 && formData.append("image3", image3);
-      image4 && formData.append("image4", image4);
+      if (image1) formData.append("image1", image1);
+      if (image2) formData.append("image2", image2);
+      if (image3) formData.append("image3", image3);
+      if (image4) formData.append("image4", image4);
 
       const response = await axios.post(
         backendUrl + "/api/product/add",
