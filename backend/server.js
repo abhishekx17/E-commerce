7013bb006d6efabc0e49401d51c6d88a8708686a
Abhishek <abhishek.0x17@gmail.com>
2026-07-20 21:47:42 +0530

    bug fixed 1.0

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const parseOrigins = (origins = '') =>
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://velora-rho-one.vercel.app',
     ...parseOrigins(process.env.CORS_ORIGINS)
 ]
 
